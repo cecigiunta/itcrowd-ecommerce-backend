612b5dcb65acc15ffd1bf6a3844771decc84e5af
cecigiunta <ceciliagiunta57@gmail.com>
2022-07-20 14:57:07 -0300

    commit heroku

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-
 
 module.exports = router;
